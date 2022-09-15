86ec581f48146c89c31b0a942c009b5eb18ae7e5
olivierifederico <olivierifederico@protonmail.com>
2022-09-15 16:16:24 -0300

    query guide

---
+++ Querys/querysguide.sql
@@ -0,0 +1,38 @@
+USE ds_pi_1;
+
+
+#driverMostWins
+
+SELECT r.idDriver, d.dName, d.dSurname, count(r.rPosition) AS wins FROM result r
+JOIN driver d ON r.iddriver = d.iddriver
+WHERE rPosition = 1
+GROUP BY idDriver
+ORDER BY wins DESC
+LIMIT 1;
+
+
+#driverConsAmeBri
+
+SELECT r.idDriver, d.dName, d.dSurname, sum(r.points) as total_score FROM result r
+JOIN Driver d on r.idDriver = d.iddriver
+JOIN Constructor c on r.idConstructor = c.idConstructor
+WHERE c.nationality = 'British' or c.nationality = 'American'
+GROUP BY r.idDriver
+ORDER BY total_score DESC
+LIMIT 1;
+
+
+#yearMostRaced
+
+SELECT yr, count(idRace) AS races FROM Race
+GROUP BY yr
+ORDER BY races DESC
+LIMIT 1;
+
+
+#trackMostRaced
+
+SELECT rName, COUNT(idRace) AS races FROM Race
+GROUP BY idTrack
+ORDER BY races DESC
+LIMIT 1;
